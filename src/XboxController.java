cdfc3090c61d9ba44c0238d2281636e9f2073e1c
Burak Turan <buraktturann@gmail.com>
2023-08-06 22:34:10 +0300

    first commit

---
+++ src/XboxController.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class XboxController implements GameController {
+    private List<ControllerButton> buttons;
+
+    public List<ControllerButton> getButtons() {
+        return buttons;
+    }
+
+    public void setButtons(List<ControllerButton> buttons) {
+        this.buttons = buttons;
+    }
+
+}
